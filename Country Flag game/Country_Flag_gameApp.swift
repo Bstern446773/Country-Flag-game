ab09c6a4e6f193beb0b59b39e3fcd618e198aa5f
Bennett Stern <bstern@bsd220.org>
2024-01-09 09:35:28 -0600

    Initial Commit

---
+++ Country Flag game/Country_Flag_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_gameApp.swift
+//  Country Flag game
+//
+//  Created by Bennett Stern on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
